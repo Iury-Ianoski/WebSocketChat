3a034930b3fdeccff34857f9137e8bc1bae79e42
Moisés Matheus B. Gonçalves <moisesmatheusbg@gmail.com>
2025-09-16 22:57:37 -0300

    parte 3 da atividade e estilizacao

--- src/public/index.js
+++ src/public/index.js
@@ -1,19 +1,61 @@
 var socket = io();
 
-const message = document.getElementById('messages')
-const text = document.getElementById('text')
-const form = document.getElementById('form');
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const messages = document.getElementById("messages");
+const usernameInput = document.getElementById("username");
+const joinBtn = document.getElementById("joinBtn");
+const sendBtn = form.querySelector("button");
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    if(text.value.trim()){
-        socket.emit('message', text.value)
-        text.value = "";
+
+let username = null;
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!username) {
+      alert("Conecte-se primeiro para poder enviar mensagens!");
+      return;
     }
-})
-
-socket.on('message', (msg) =>{
-    const li = document.createElement('li');
-    li.textContent = msg
-    message.appendChild(li)
+    if (username && text.value.trim()) {
+    socket.emit("message", text.value);
+    text.value = "";
+    }
 });
+
+socket.on("message", addMessage);
+
+socket.on("messageHistory", (history) => {
+    history.forEach(addMessage);
+});
+
+function addMessage(msg) {
+    const li = document.createElement("li");
+    li.textContent = `${msg.user}: ${msg.text}`;
+
+    if (msg.user === username) {
+        li.classList.add("eu");
+    } else if (msg.user === 'Sistema') {
+        li.classList.add("sistema")
+    } else{
+        li.classList.add("outro");
+    }
+
+    messages.appendChild(li);
+    messages.scrollTop = messages.scrollHeight;
+}
+
+joinBtn.onclick = () => {
+username = usernameInput.value.trim();
+    if (username) {
+      socket.emit("setUsername", username);
+      joinBtn.disabled = true;
+      usernameInput.disabled = true;
+
+      text.disabled = false;
+      sendBtn.disabled = false;
+      text.focus();
+    } else {
+      alert("informe um nome antes de entrar no chat.");
+    }
+};
+
--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,10 @@ class App{
     private http: http.Server
     private io: Server
 
+    private users: Record<string, string> = {}
+    private messageHistory: { user: string, text: string }[] = []
+    private MAX_HISTORY = 20
+
     constructor(){
         this.app = express()
         this.http = http.createServer(this.app)
@@ -20,15 +24,52 @@ class App{
         this.routes()
     }
     listenServer(){
-        this.http.listen(3000, ()=> console.log('Tá rodando'))
+        this.http.listen(3000, ()=> console.log('Está rodando em http://localhost:3000'))
     }
-    listenSocket(){
-        this.io.on('connection', (Socket) =>{
-            console.log('Usuário conectado: ', Socket.id)
-            
-            Socket.on('message', (msg) =>{
-                this.io.emit('message', msg)
-            });
+    listenSocket() {
+        this.io.on('connection', (socket: Socket) => {
+            console.log('Usuário conectado:', socket.id)
+
+            //usuário entra com nome
+            socket.on('setUsername', (username: string) => {
+                this.users[socket.id] = username
+                console.log(`${username} entrou no chat.`)
+
+                socket.emit('messageHistory', this.messageHistory)
+
+                this.io.emit('message', {
+                    user: "Sistema",
+                    text: `${username} entrou no chat`
+                });
+            })
+
+            //Envio de mensagens
+            socket.on('message', (msg: string) => {
+                const message = {
+                    user: this.users[socket.id],
+                    text: msg
+                }
+
+                this.messageHistory.push(message)
+                if (this.messageHistory.length > this.MAX_HISTORY) {
+                    this.messageHistory.shift()
+                }
+
+                this.io.emit('message', message)
+            })
+
+            //Usuário desconecta
+            socket.on('disconnect', () => {
+                const username = this.users[socket.id]
+                delete this.users[socket.id]
+
+                if (username) {
+                    this.io.emit('message', {
+                        user: "Sistema",
+                        text: `${username} saiu do chat`
+                    })
+                }
+            })
         })
     }
     configEjs(){
